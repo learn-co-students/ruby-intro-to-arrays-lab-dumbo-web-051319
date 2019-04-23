e1cac69abc1e1454d3639fe9f40c02b6b19be8b7
DDuongNguyen <duong.nguyen4991@gmail.com>
2019-04-24 00:25:00 +0700

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  ["popo", "momo"]
+end
+
+def first_element(cat)
+  cat[0]
+end
+
+def third_element(cat)
+  cat[2]
+end
+
+def last_element(cat)
+  cat[-1]
+end
+
+def first_element_with_array_methods(cat)
+  cat.first
+end
+
+def last_element_with_array_methods(cat)
+  cat.last
+end
+
+def length_of_array(cat)
+  cat.count
+end
