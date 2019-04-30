2bb30cfaa4234f7c9fc12de20357fe477b217b68
Lorena Ramirez-Lopez <daleloreny@gmail.com>
2019-04-30 20:45:57 +0000

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,6 +7,27 @@ def array_with_two_elements
   [1,2]
 end
 
-#def first_element
+def first_element
   
-#end
+end
+
+def third_element
+  
+end
+
+def last_element
+  
+end
+
+def first_element_with_array_methods
+
+end
+
+def last_element_with_array_methods
+
+end
+
+def length_of_array
+  
+end
+
